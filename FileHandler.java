7b5ed57625c57aa17ff3ff37e04e9e084eba3158
HanumRoslan <ah.roslan@live.iium.edu.my>
2026-06-10 17:05:10 +0800

    Update FileHandler.java

--- FileHandler.java
+++ FileHandler.java
@@ -11,61 +11,51 @@ public class FileHandler {
      
        // save pets into file ( fav file of each user ) 
        try {
-         
-           // create file
-           FileWriter fw = new FileWriter("favourites.txt");
+            // create file
+             FileWriter fw = new FileWriter("favourites.txt");
            
-           for(FavouritePage f : favouriteList){ // loop 
+            for(FavouritePage f : favouriteList){ // loop 
                fw.write(f.getUserID() + "," + f.getPetID() + "\n"); // save 
-           }
+            }
            
            fw.close();
            
            System.out.println("Favourites saved!");
            
-       } catch (IOException e){
+        } catch (IOException e){
            System.out.println("Error saving favourites.."); // display errors if saving fails
-       }
-   } 
+        }
+    } 
    
-   // load fav pets // search 
-   public ArrayList<FavouritePage> loadFav(){
-       
+    // load fav pets // search 
+    public ArrayList<FavouritePage> loadFav(){
        ArrayList<FavouritePage> favList = new ArrayList<>();
        
        try {
-           
            BufferedReader br = new BufferedReader(new FileReader("favourites.txt"));
-           
            String line;
-           
+
            while((line = br.readLine()) != null){
                String[] data = line.split(",");
                
                FavouritePage fav = new FavouritePage(data[0], data[1]);
                favList.add(fav);
-           }
-           
+            }
            br.close();
-           
-       } catch(IOException e) {
+        } catch(IOException e) {
            System.out.println("Error loading..");
-       }
-       
-       return favList;
-   }
+        }
+        return favList;
+    }
    
    // save admin logs 
-   
    public void saveAdminLog(String actions) {
-       
        try{
-           
            FileWriter fw = new FileWriter("adminLogs.txt" , true);
            fw.write(actions + "\n");
            fw.close();
-       }catch(IOException e){
+        } catch(IOException e) {
            System.out.println("Error saving logs..");
-       }
-   }
+        }
+    }
 }
