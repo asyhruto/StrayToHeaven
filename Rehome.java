e780d51761fbd49fee03cb27d6baeeb6e110666a
HanumRoslan <ah.roslan@live.iium.edu.my>
2026-06-10 17:08:09 +0800

    Update Rehome.java

--- Rehome.java
+++ Rehome.java
@@ -1,16 +1,15 @@
-package com.mycompany.ooprehome;
+package com.example;
 
 public class Rehome {
-
-private String rehomeID;
-private String userID;
-private String petName;
-private int petAge;
-private String petBreed;
-private String petGender;
-private String petTraits;
+    private String rehomeID;
+    private String userID;
+    private String petName;
+    private int petAge;
+    private String petBreed;
+    private String petGender;
+    private String petTraits;
     
-// Constructor to initialize the Rehome request form
+    // Constructor to initialize the Rehome request form
     public Rehome (String rehomeID, String userID, String petName, String petBreed, int petAge, String petGender, String petTraits) {
         this.rehomeID = rehomeID;
         this.userID = userID;
@@ -21,16 +20,13 @@ private String petTraits;
         this.petTraits = petTraits;       
     }
     
-// Prints a confirmation message that the rehome application has been submitted  
+    // Prints a confirmation message that the rehome application has been submitted  
     public void submitPet() {
         System.out.println("Confirmation: Rehome application for '" + petName + "' has been successfully submitted!");
     }
 
-// Returns a formatted text string containing the request ID and pet name
+    // Returns a formatted text string containing the request ID and pet name
     public String getRehomeDetails() {
         return "Request ID: " + rehomeID + " | Pet Name: " + petName;
     }
 }
-
-
-
